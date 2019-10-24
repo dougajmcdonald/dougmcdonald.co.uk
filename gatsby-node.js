4759d53545377932e63b89968d5aef241b4ec385
Doug McDonald <dougajmcdonald@gmail.com>
2019-10-24 21:26:27 +0100

    Add post to avoid errors

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,48 +1,35 @@
 const path = require(`path`)
 
-exports.createPages = ({ graphql, actions }) => {
+exports.createPages = ({ actions, graphql }) => {
   const { createPage } = actions
+
   const blogPostTemplate = path.resolve(`src/templates/blog-post.js`)
-  // Query for markdown nodes to use in creating pages.
-  // You can query for whatever data you want to create pages for e.g.
-  // products, portfolio items, landing pages, etc.
-  // Variables can be added as the second function parameter
-  return graphql(
-    `
-      query loadPagesQuery($limit: Int!) {
-        allMarkdownRemark(limit: $limit) {
-          edges {
-            node {
-              frontmatter {
-                slug
-              }
+
+  return graphql(`
+    {
+      allMarkdownRemark(
+        sort: { order: DESC, fields: [frontmatter___date] }
+        limit: 1000
+      ) {
+        edges {
+          node {
+            frontmatter {
+              path
             }
           }
         }
       }
-    `,
-    { limit: 1000 }
-  ).then(result => {
+    }
+  `).then(result => {
     if (result.errors) {
-      throw result.errors
+      return Promise.reject(result.errors)
     }
 
-    // Create blog post pages.
-    result.data.allMarkdownRemark.edges.forEach(edge => {
+    return result.data.allMarkdownRemark.edges.forEach(({ node }) => {
       createPage({
-        // Path for this page — required
-        path: `${edge.node.frontmatter.slug}`,
+        path: node.frontmatter.path,
         component: blogPostTemplate,
-        context: {
-          // Add optional context data to be inserted
-          // as props into the page component..
-          //
-          // The context data can also be used as
-          // arguments to the page GraphQL query.
-          //
-          // The page "path" is always available as a GraphQL
-          // argument.
-        },
+        context: {}, // additional data can be passed via context
       })
     })
   })

982a73d7676f6d3e7c0e8a775e22fc2540451674
Doug McDonald <dougajmcdonald@gmail.com>
2019-10-24 21:20:45 +0100

    update gatsby-node graphql query

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,38 +1,49 @@
 const path = require(`path`)
 
-exports.createPages = async ({ actions, graphql, reporter }) => {
+exports.createPages = ({ graphql, actions }) => {
   const { createPage } = actions
-
-  const blogPostTemplate = path.resolve(`src/templates/blogTemplate.js`)
-
-  const result = await graphql(`
-    {
-      allMarkdownRemark(
-        sort: { order: DESC, fields: [frontmatter___date] }
-        limit: 1000
-      ) {
-        edges {
-          node {
-            frontmatter {
-              path
+  const blogPostTemplate = path.resolve(`src/templates/blog-post.js`)
+  // Query for markdown nodes to use in creating pages.
+  // You can query for whatever data you want to create pages for e.g.
+  // products, portfolio items, landing pages, etc.
+  // Variables can be added as the second function parameter
+  return graphql(
+    `
+      query loadPagesQuery($limit: Int!) {
+        allMarkdownRemark(limit: $limit) {
+          edges {
+            node {
+              frontmatter {
+                slug
+              }
             }
           }
         }
       }
+    `,
+    { limit: 1000 }
+  ).then(result => {
+    if (result.errors) {
+      throw result.errors
     }
-  `)
 
-  // Handle errors
-  if (result.errors) {
-    reporter.panicOnBuild(`Error while running GraphQL query.`)
-    return
-  }
-
-  result.data.allMarkdownRemark.edges.forEach(({ node }) => {
-    createPage({
-      path: node.frontmatter.path,
-      component: blogPostTemplate,
-      context: {}, // additional data can be passed via context
+    // Create blog post pages.
+    result.data.allMarkdownRemark.edges.forEach(edge => {
+      createPage({
+        // Path for this page — required
+        path: `${edge.node.frontmatter.slug}`,
+        component: blogPostTemplate,
+        context: {
+          // Add optional context data to be inserted
+          // as props into the page component..
+          //
+          // The context data can also be used as
+          // arguments to the page GraphQL query.
+          //
+          // The page "path" is always available as a GraphQL
+          // argument.
+        },
+      })
     })
   })
 }

f01b1266992c927c275863e466d27668c5d55634
Doug McDonald <dougajmcdonald@gmail.com>
2019-12-03 10:42:45 +0000

    Removed top margin

--- src/components/postlink.js
+++ src/components/postlink.js
@@ -14,7 +14,7 @@ const PostLink = ({ post: { excerpt, frontmatter } }) => (
       height={frontmatter.image.childImageSharp.resize.height}
       className="inline-block mr-sm lg:mr-3xl"
     />
-    <div className="flex flex-col inline-block mt-base md:w-2/3">
+    <div className="flex flex-col inline-block md:w-2/3">
       <span className="date">{frontmatter.date}</span>
       <h3 className="post-title">{frontmatter.title}</h3>
       <span className="inline-block w-5xl h-xxxs bg-primary mb-lg"></span>

0b89e5aeb90bcdce1ca927bb5b50face1bd8bd14
Doug McDonald <dougajmcdonald@gmail.com>
2019-12-27 16:28:23 +0000

    add manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,6 +18,24 @@ module.exports = {
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
     {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "dougmcdonald.co.uk",
+        short_name: "dougmcdonald.co.uk",
+        start_url: "/",
+        background_color: "#fffff",
+        theme_color: "#2DCACA",
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: "standalone",
+        icon: "src/images/favicon.png", // This path is relative to the root of the site.
+        // An optional attribute which provides support for CORS check.
+        // If you do not provide a crossOrigin option, it will skip CORS for manifest.
+        // Any invalid keyword or empty string defaults to `anonymous`
+        crossOrigin: `use-credentials`,
+      },
+    },
+    {
       resolve: `gatsby-transformer-remark`,
       options: {
         plugins: [

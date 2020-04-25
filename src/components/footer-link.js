e912223af5d4328a8da1814e62b9e2db3c22c663
Doug McDonald <dougajmcdonald@gmail.com>
2020-04-25 09:37:40 +0100

    small margin adjustment

--- src/components/footer-link.js
+++ src/components/footer-link.js
@@ -1,9 +1,9 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from 'react'
+import { Link } from 'gatsby'
 
 export default ({ path, text }) => (
   <Link
-    className="mr-lg ml-lg font-medium leading-loose border-b-2 border-transparent hover:border-primary"
+    className="mx-lg font-medium leading-loose border-b-2 border-transparent hover:border-primary"
     to={path}
   >
     {text}
